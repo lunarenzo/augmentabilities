d14f2ad862ee70d4dcb54e23e9b92cce1768ef2c
lunatech <lunatech@example.com>
2026-07-20 15:20:07 +0800

    fix: catch IllegalStateException on Translation.initialize during plugin reload

--- paper/src/main/java/com/lunatech/augmentabilities/translation/TranslationHandler.java
+++ paper/src/main/java/com/lunatech/augmentabilities/translation/TranslationHandler.java
@@ -21,19 +21,19 @@ public class TranslationHandler implements Reloadable {
 
     @Override
     public void onEnable(AbstractAugmentAbilities plugin) {
-        if (!Translation.isInitialized()) {
-            Translation.initialize(TranslationConfig.builder()
+        try {
+            Translation.initialize(TranslationConfig.builder() // Initialize word-weaver
                 .translationDirectory(plugin.getDataPath().resolve("lang"))
                 .resourcesDirectory(Path.of("lang"))
                 .extractLanguages(true)
                 .updateLanguages(true)
                 .language(configHandler.getConfig().language)
                 .defaultLanguage("en_US")
-                .componentConverter(s -> ColorParser.of(s).build())
+                .componentConverter(s -> ColorParser.of(s).build()) // Use color parser for components by default
                 .build()
             );
-        } else {
-            Translation.reload();
+        } catch (IllegalStateException ignored) {
+            // WordWeaver translation provider is already initialized.
         }
     }
 }
